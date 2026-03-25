b33401dfcf83139139c6f7719ad02a53862e713b
Maciej Bry <maciej.bryja@wp.pl>
2026-03-25 19:30:24 +0100

    feat: extend ThermoRecorderRepository with JpaSpecificationExecutor

--- src/main/java/com/mac/bry/validationsystem/thermorecorder/ThermoRecorderRepository.java
+++ src/main/java/com/mac/bry/validationsystem/thermorecorder/ThermoRecorderRepository.java
@@ -7,13 +7,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ThermoRecorderRepository extends JpaRepository<ThermoRecorder, Long> {
+public interface ThermoRecorderRepository extends JpaRepository<ThermoRecorder, Long>, JpaSpecificationExecutor<ThermoRecorder> {
 
         Optional<ThermoRecorder> findBySerialNumber(String serialNumber);
 
